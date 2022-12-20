ecf549f4d3c988fcf44b34402cdb534c71881056
Rui Coelho <ruicoelhopedro@gmail.com>
2022-12-20 14:28:02 +0000

    Update check extensions logic
    STC: LLR:  2.97/2.94<0.00, 6.00> Elo diff: 4.10 [0.81, 7.40] (95%)
    Games: 17440 W: 3743 L: 3537 D: 10160 Draw ratio: 58.3%
    Pntl: [351, 2031, 3769, 2199, 370]
    
    LTC: LLR:  2.96/2.94<0.00, 6.00> Elo diff: 6.21 [1.73, 10.70] (95%)
    Games: 7718 W: 1389 L: 1251 D: 5078 Draw ratio: 65.8%
    Pntl: [107, 790, 1933, 916, 113]
    
    Bench: 2157555

--- src/Search.cpp
+++ src/Search.cpp
@@ -534,7 +534,7 @@ namespace Search
             position.make_move(move);
 
             // Check extensions
-            if (InCheck && depth < 4)
+            if (!extension && position.in_check() && abs(static_eval) > 75)
                 extension = 1;
 
             // Update depth and search data
@@ -555,7 +555,7 @@ namespace Search
                               - (move_score + 15000) / 30000;
 
                 // Reduced depth search
-                Depth new_depth = reduce(depth, 1 + std::max(0, reduction));
+                Depth new_depth = reduce(curr_depth, 1 + std::max(0, reduction));
                 score = -negamax<NON_PV>(position, new_depth, -alpha - 1, -alpha, curr_data);
 
                 // Only carry a full search if this reduced move fails high
